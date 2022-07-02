e145383e9b22c7707585630e4f3a6c0a695a63f8
serghine <sergh.boudj@gmail.com>
2022-07-02 13:29:07 +0200

    [projet vuex learn mutationns getters store....]

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {},
+  data(){
+    return{
+      counter:0
+    }
+  }
+
+}
+</script>
